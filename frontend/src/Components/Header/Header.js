cf4a7c05e29072880adf9a386ad5744c28172332
mohadese qanaatgar <m.qanaatgar@gmail.com>
2023-12-14 21:10:43 +0330

    add landing

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Header.css';
 import Topbar from '../Topbar/Topbar';
 import Navbar from '../Navbar/Navbar';
+import Landing from '../Landing/Landing';
 
 export default function Header() {
   return (
     <header className="header">
       <Topbar />
       <Navbar />
+      <Landing/>
     </header>
   );
 }
