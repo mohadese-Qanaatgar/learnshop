c7d16dcaf02c5d146333c56ad108b0b1d8a87dc6
mohadese qanaatgar <m.qanaatgar@gmail.com>
2023-12-14 17:39:52 +0330

    inital routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React  from 'react'
+import {useRoutes} from 'react-router-dom'
+import routes from './routes'
 
 export default function App() {
+
+ const router = useRoutes(routes)
+
   return (
-    <div>App</div>
+    <div>App{router}</div>
   )
 }
