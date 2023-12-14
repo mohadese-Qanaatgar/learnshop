fa69874262ed2c57c2fc1ea953d065bdd64402e5
mohadese qanaatgar <m.qanaatgar@gmail.com>
2023-12-15 00:06:28 +0330

    feat lastCourses

--- frontend/src/Components/SectionHeader/SectionHeader.js
+++ frontend/src/Components/SectionHeader/SectionHeader.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './SectionHeader.css';
 
-export default function SectionHeader() {
+export default function SectionHeader({ title, desc, btnTitle }) {
   return (
     <div class="courses-header">
       <div class="courses-header__right">
-        <span class="courses-header__title title">جدیدترین دوره ها</span>
-        <span class="courses-header__text">سکوی پرتاپ شما به سمت موفقیت</span>
+        <span class="courses-header__title title">{title}</span>
+        <span class="courses-header__text">{desc}</span>
       </div>
-      <div class="courses-header__left">
-        <a href="#" class="courses-header__link">
-          تمامی دوره ها
-          <i class="fas fa-arrow-left courses-header__icon"></i>
-        </a>
-      </div>
+      {btnTitle.length !== 0 ? (
+        <div class="courses-header__left">
+          <a href="#" class="courses-header__link">
+            {btnTitle}
+            <i class="fas fa-arrow-left courses-header__icon"></i>
+          </a>
+        </div>
+      ) : null}
     </div>
   );
 }
--- frontend/src/Pages/Index/Index.js
+++ frontend/src/Pages/Index/Index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Index.css'
 import Header from '../../Components/Header/Header'
+import LastCourses from '../../Components/LastCourses/LastCourses'
 
 export default function Index() {
   return (
     <>
     <Header/>
-    
+    <LastCourses/>
     </>
   )
 }
