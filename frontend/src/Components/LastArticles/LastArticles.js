77713e69e7e9680f99258bb7aa20f95d48a86ce0
mohadese qanaatgar <m.qanaatgar@gmail.com>
2023-12-15 19:49:57 +0330

    feat Article

--- frontend/src/Components/LastArticles/LastArticles.js
+++ frontend/src/Components/LastArticles/LastArticles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LastArticles.css';
 import SectionHeader from '../SectionHeader/SectionHeader';
+import ArticleBox from '../ArticleBox/ArticleBox';
 
 export default function LastArticles() {
   return (
@@ -11,6 +12,28 @@ export default function LastArticles() {
           desc="پیش به سوی ارتقای دانش"
           btnTitle="تمامی مقاله ها"
         />
+        <div class="articles__content">
+          <div class="row">
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه
+            های مختلفی برای تسریع..."
+              cover="images/blog/3.jpg"
+            />
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه
+            های مختلفی برای تسریع..."
+              cover="images/blog/3.jpg"
+            />
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه
+            های مختلفی برای تسریع..."
+              cover="images/blog/3.jpg"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
