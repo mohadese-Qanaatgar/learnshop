4267bcc8f9feee8a8cd35308731efe15a73851d9
mohadese qanaatgar <m.qanaatgar@gmail.com>
2023-12-17 00:30:15 +0330

    build Login and Register

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,17 @@ import CourseInfo from './Pages/CourseInfo/CourseInfo'
 import Category from './Pages/Category/Category'
 import ArticleInfo from './Pages/ArticleInfo/ArticleInfo'
 import Courses from './Pages/Courses/Courses'
+import Login from './Pages/Login/Login'
+import Register from './Pages/Register/Register'
 
 const routes = [
     {path:'/' , element :<Index/>},
     {path :'/course-info/:courseName' , element:<CourseInfo/>},
     {path:'/category-info/:categoryName' , element :<Category/>},
     {path:'/article-info/:articleName' , element : <ArticleInfo/>},
-    {path:'/courses' , element : <Courses/>}
+    {path:'/courses' , element : <Courses/>},
+    {path:'/login' , element : <Login/>},
+    {path:'/register' , element : <Register/>}
 ]
 
 export default routes
