77b3457af129054ac491cbc2f8f057b84b73ba80
mohadese qanaatgar <m.qanaatgar@gmail.com>
2023-12-17 14:33:28 +0330

    update Input component

--- frontend/src/Components/Form/Input.js
+++ frontend/src/Components/Form/Input.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Input.css';
 
 export default function input(props) {
+
+    const onChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
+
   const element =
     props.element === 'input' ? (
       <input
         type={props.type}
         placeholder={props.placeholder}
         className={props.className}
+        onChange={onChangeHandler}
       />
     ) : (
-      <textarea placeholder={props.placeholder} className={props.className} />
+      <textarea placeholder={props.placeholder} className={props.className}
+      onChange={onChangeHandler} />
     );
 
   return (
