59fd201c1cdd7f285949895881236a78551d4af4
unknown <m.qanaatgar@gmail.com>
2024-02-02 13:45:31 +0330

    show admin notifications

--- frontend/src/Components/AdminPanel/Sidebar/Sidebar.js
+++ frontend/src/Components/AdminPanel/Sidebar/Sidebar.js
@@ -4,21 +4,21 @@ import './Sidebar.css'
 
 export default function Sidebar() {
   return (
-    <div id="sidebar" class="col-2">
-      <div class="sidebar-header">
-        <div class="sidebar-logo">
+    <div id="sidebar" className="col-2">
+      <div className="sidebar-header">
+        <div className="sidebar-logo">
           <a href="#">
             <img src="/images/logo/Logo.png" alt="Logo" />
           </a>
         </div>
 
-        <div class="sidebar-menu-btn">
-          <i class="fas fa-bars"></i>
+        <div className="sidebar-menu-btn">
+          <i className="fas fa-bars"></i>
         </div>
       </div>
-      <div class="sidebar-menu">
+      <div className="sidebar-menu">
         <ul>
-          <li class="active-menu">
+          <li className="active-menu">
             <Link to='/p-admin'>
               <span>صفحه اصلی</span>
             </Link>
--- frontend/src/Components/AdminPanel/Topbar/Topbar.js
+++ frontend/src/Components/AdminPanel/Topbar/Topbar.js
@@ -1,77 +1,74 @@
 import React, { useEffect, useState } from "react";
 
 export default function Topbar() {
+  const [adminInfo, setAdminInfo] = useState({});
+  const [adminNotifications, setAdminNotifications] = useState([]);
+  const [isShowNotificationsBox, setIsShowNotificationsBox] = useState(false);
 
-  const [adminInfo , setAdminInfo] =useState({})
   useEffect(() => {
-    const localStorageData = JSON.parse(localStorage.getItem('user'))
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
     // console.log(localStorageData);
 
-    fetch(`http://localhost:4000/v1/auth/me`,{
-      headers :{
-        'Authorization' : `Bearer ${localStorageData.token}`
-      }
-    }).then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setAdminInfo(data)
+    fetch(`http://localhost:4000/v1/auth/me`, {
+      headers: {
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
     })
-  } , [])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAdminInfo(data);
+        setAdminNotifications(data.notifications);
+      });
+  }, []);
   return (
-    <div class="container-fluid">
-      <div class="container">
-        <div class="home-header">
-          <div class="home-right">
-            <div class="home-searchbar">
-              <input type="text" class="search-bar" placeholder="جستجو..." />
+    <div className="container-fluid">
+      <div className="container">
+        <div className={`home-header ${isShowNotificationsBox ? 'active-modal-notfication' : ''}`}>
+          <div className="home-right">
+            <div className="home-searchbar">
+              <input type="text" className="search-bar" placeholder="جستجو..." />
             </div>
-            <div class="home-notification">
-              <button type="button">
-                <i class="far fa-bell"></i>
+            <div className="home-notification">
+              <button
+                type="button"
+                onMouseEnter={() => setIsShowNotificationsBox(true)}
+              >
+                <i className="far fa-bell"></i>
               </button>
             </div>
-            <div class="home-notification-modal">
-              <ul class="home-notification-modal-list">
-                <li class="home-notification-modal-item">
-                  <span class="home-notification-modal-text">پیغام ها</span>
-                  <label class="switch">
-                    <input type="checkbox" checked />
-                    <span class="slider round"></span>
-                  </label>
-                </li>
-                <li class="home-notification-modal-item">
-                  <span class="home-notification-modal-text">پیغام ها</span>
-                  <label class="switch">
-                    <input type="checkbox" checked />
-                    <span class="slider round"></span>
-                  </label>
-                </li>
-                <li class="home-notification-modal-item">
-                  <span class="home-notification-modal-text">پیغام ها</span>
-                  <label class="switch">
-                    <input type="checkbox" checked />
-                    <span class="slider round"></span>
-                  </label>
-                </li>
-                <li class="home-notification-modal-item">
-                  <span class="home-notification-modal-text">پیغام ها</span>
-                  <label class="switch">
-                    <input type="checkbox" checked />
-                    <span class="slider round"></span>
-                  </label>
-                </li>
+            <div className="home-notification-modal"
+            onMouseEnter={() => setIsShowNotificationsBox(true)}
+            onMouseLeave={() => setIsShowNotificationsBox(false)}>
+              <ul className="home-notification-modal-list">
+                {adminNotifications.length === 0 ? (
+                  <div>نوتیفی وجود ندارد</div>
+                ) : (
+                  <>
+                    {adminNotifications.map((notification) => (
+                      <>
+                        <li className="home-notification-modal-item">
+                          <span className="home-notification-modal-text">
+                            {notification}
+                          </span>
+                          <label className="switch">
+                            <a href="javascript:void(0)">دیدم</a>
+                          </label>
+                        </li>
+                      </>
+                    ))}
+                  </>
+                )}
               </ul>
             </div>
           </div>
-          <div class="home-left">
-            <div class="home-profile">
-              <div class="home-profile-name">
-                <a href="#">
-                  {adminInfo.name}
-                  </a>
+          <div className="home-left">
+            <div className="home-profile">
+              <div className="home-profile-name">
+                <a href="#">{adminInfo.name}</a>
               </div>
-              <div class="home-profile-icon">
-                <i class="fas fa-angle-down"></i>
+              <div className="home-profile-icon">
+                <i className="fas fa-angle-down"></i>
               </div>
             </div>
           </div>
