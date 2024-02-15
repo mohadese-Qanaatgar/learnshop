24062eff98ba1de51abb7bdbdd9244148402cfbe
unknown <m.qanaatgar@gmail.com>
2024-02-15 22:50:38 +0330

    remove course in pAdmin

--- frontend/src/Pages/AdminPanel/Courses/Courses.js
+++ frontend/src/Pages/AdminPanel/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from '../DataTable/DataTable'
+import swal from 'sweetalert'
 
 export default function Courses() {
 
@@ -7,7 +8,11 @@ export default function Courses() {
 
 
   useEffect(() => {
-    const localStorageData = JSON.parse(localStorage.getItem('user'))
+    getAllCourses()
+  } ,[])
+
+  function getAllCourses () {
+const localStorageData = JSON.parse(localStorage.getItem('user'))
 
     fetch(`http://localhost:4000/v1/courses`,{
       headers :{
@@ -18,7 +23,33 @@ export default function Courses() {
       console.log(allCourses);
       setCourses(allCourses)
     })
-  } ,[])
+  }
+
+  const removeCourse = (courseID) => {
+    swal({
+      title : 'آیا مطمئن هستید میخواهید حذف کنید؟',
+      icon : 'warning',
+      buttons : ["نه","آره"]
+    }).then(result => {
+      if(result) {
+        const localStorageData = JSON.parse(localStorage.getItem('user'))
+
+        fetch(`http://localhost:4000/v1/courses/${courseID}` , {
+          method : 'DELETE',
+          headers : {
+            Authorization : `Bearer ${localStorageData.token}`
+          }
+        }).then(res => res.json())
+        .then(result => {
+          swal({
+            title : 'دوره با موفقیت حذف شد',
+            icon : 'success',
+            buttons : 'تایید'
+          }).then(() => getAllCourses())
+        })
+      }
+    })
+  }
   return (
    <>
    <DataTable title='دوره ها'>
@@ -55,7 +86,7 @@ export default function Courses() {
                   <button
                     type="button"
                     class="btn btn-danger delete-btn"
-                   
+                   onClick={() => removeCourse(course._id)}
                   >
                     حذف
                   </button>
