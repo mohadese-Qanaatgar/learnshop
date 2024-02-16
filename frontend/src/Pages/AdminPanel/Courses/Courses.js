d370d1f836c4b1282e27212c18fa87c5e5a82f37
unknown <m.qanaatgar@gmail.com>
2024-02-17 00:23:40 +0330

    preparation add new course form

--- frontend/src/Pages/AdminPanel/Courses/Courses.js
+++ frontend/src/Pages/AdminPanel/Courses/Courses.js
@@ -1,113 +1,181 @@
-import React, { useEffect, useState } from 'react'
-import DataTable from '../DataTable/DataTable'
-import swal from 'sweetalert'
-import './Courses.css'
+import React, { useEffect, useState } from "react";
+import DataTable from "../DataTable/DataTable";
+import swal from "sweetalert";
+import { useForm } from "../../../hooks/useForm";
+import Input from "../../../Components/Form/Input";
+import {
+  requiredValidator,
+  minValidator,
+  maxValidator,
+} from "../../../Validators/rules";
+import "./Courses.css";
 
 export default function Courses() {
-
-  const [courses , setCourses] = useState([])
+  const [courses, setCourses] = useState([]);
   const [courseCategory, setCourseCategory] = useState("");
   const [categories, setCategories] = useState([]);
+  const [courseStatus , setCourseStatus] = useState('start')
+  const [courseCover ,setCourseCover] = useState({})
+
+  const [formState, onInputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      shortName: {
+        value: "",
+        isValid: false,
+      },
+      price: {
+        value: "",
+        isValid: false,
+      },
+      support: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
   useEffect(() => {
-    getAllCourses()
+    getAllCourses();
     fetch(`http://localhost:4000/v1/category`)
-    .then((res) => res.json())
-    .then((allCategories) => {
-      setCategories(allCategories);
-    });
-  } ,[])
+      .then((res) => res.json())
+      .then((allCategories) => {
+        setCategories(allCategories);
+      });
+  }, []);
 
-  function getAllCourses () {
-const localStorageData = JSON.parse(localStorage.getItem('user'))
+  function getAllCourses() {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
 
-    fetch(`http://localhost:4000/v1/courses`,{
-      headers :{
-        Authorization : `Bearer ${localStorageData.token}`
-      }
-    }).then(res => res.json())
-    .then(allCourses => {
-      console.log(allCourses);
-      setCourses(allCourses)
+    fetch(`http://localhost:4000/v1/courses`, {
+      headers: {
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
     })
+      .then((res) => res.json())
+      .then((allCourses) => {
+        // console.log(allCourses);
+        setCourses(allCourses);
+      });
   }
 
   const removeCourse = (courseID) => {
     swal({
-      title : 'آیا مطمئن هستید میخواهید حذف کنید؟',
-      icon : 'warning',
-      buttons : ["نه","آره"]
-    }).then(result => {
-      if(result) {
-        const localStorageData = JSON.parse(localStorage.getItem('user'))
+      title: "آیا مطمئن هستید میخواهید حذف کنید؟",
+      icon: "warning",
+      buttons: ["نه", "آره"],
+    }).then((result) => {
+      if (result) {
+        const localStorageData = JSON.parse(localStorage.getItem("user"));
 
-        fetch(`http://localhost:4000/v1/courses/${courseID}` , {
-          method : 'DELETE',
-          headers : {
-            Authorization : `Bearer ${localStorageData.token}`
-          }
-        }).then(res => res.json())
-        .then(result => {
-          swal({
-            title : 'دوره با موفقیت حذف شد',
-            icon : 'success',
-            buttons : 'تایید'
-          }).then(() => getAllCourses())
+        fetch(`http://localhost:4000/v1/courses/${courseID}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${localStorageData.token}`,
+          },
         })
+          .then((res) => res.json())
+          .then((result) => {
+            swal({
+              title: "دوره با موفقیت حذف شد",
+              icon: "success",
+              buttons: "تایید",
+            }).then(() => getAllCourses());
+          });
       }
-    })
-  }
+    });
+  };
   const selectCategory = (event) => {
     setCourseCategory(event.target.value);
   };
+
+  const addNewCourse = (event) => {
+    event.preventDefault()
+    console.log(formState);
+  }
+
   return (
-   <>
-   <div class="container-fluid" id="home-content">
+    <>
+      <div class="container-fluid" id="home-content">
         <div class="container">
           <div class="home-title">
-            <span>افزودن محصول جدید</span>
+            <span>افزودن دوره جدید</span>
           </div>
           <form class="form">
             <div class="col-6">
               <div class="name input">
-                <label class="input-title">نام محصول</label>
-                <input
+                <label class="input-title">نام دوره</label>
+                <Input
+                  id="name"
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
                   type="text"
-                  isValid="false"
-                  placeholder="لطفا نام محصول را وارد کنید..."
+                  placeholder="لطفا نام دوره را وارد کنید..."
                 />
                 <span class="error-message text-danger"></span>
               </div>
             </div>
             <div class="col-6">
               <div class="price input">
-                <label class="input-title">قیمت محصول</label>
-                <input
+                <label class="input-title">توضیحات دوره</label>
+                <Input
+                  id="description"
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
                   type="text"
-                  isValid="false"
-                  placeholder="لطفا قیمت محصول را وارد کنید..."
+                  placeholder="لطفا توضیحات را وارد کنید..."
                 />
                 <span class="error-message text-danger"></span>
               </div>
             </div>
             <div class="col-6">
               <div class="number input">
-                <label class="input-title">تعداد محصول</label>
-                <input
+                <label class="input-title">Url دوره </label>
+                <Input
+                  id="shortName"
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
                   type="text"
-                  isValid="false"
-                  placeholder="لطفا تعداد محصول را وارد کنید..."
+                  placeholder="لطفا Url دوره را وارد کنید..."
                 />
                 <span class="error-message text-danger"></span>
               </div>
             </div>
             <div class="col-6">
               <div class="price input">
-                <label class="input-title">قیمت محصول</label>
-                <input
+                <label class="input-title">قیمت دوره</label>
+                <Input
+                  id="price"
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
                   type="text"
-                  isValid="false"
-                  placeholder="لطفا قیمت محصول را وارد کنید..."
+                  placeholder="لطفا قیمت دوره را وارد کنید..."
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="price input">
+                <label class="input-title">نحوه پشتیبانی دوره</label>
+                <Input
+                  id="support"
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  validations={[minValidator(5)]}
+                  type="text"
+                  placeholder="لطفا نحوه پشتیبانی را وارد کنید..."
                 />
                 <span class="error-message text-danger"></span>
               </div>
@@ -125,72 +193,49 @@ const localStorageData = JSON.parse(localStorage.getItem('user'))
             </div>
             <div class="col-6">
               <div class="file">
-                <label class="input-title">عکس محصول</label>
-                <input type="file" id="file" />
+                <label class="input-title">عکس دوره</label>
+                <input type="file" id="file" onChange={event => {
+                  console.log(event.target.files[0]);
+                  setCourseCover(event.target.files[0])
+                }}/>
               </div>
             </div>
             <div class="col-12">
               <div class="bottom-form">
                 <div class="condition">
-                  <label class="input-title">موجودی</label>
+                  <label class="input-title">وضعیت دوره</label>
                   <div class="radios">
                     <div class="available">
                       <label>
-                        <span>موجود</span>
+                        <span>در حال برگزاری</span>
                         <input
                           type="radio"
-                          value="avalibe"
+                          value="start"
                           name="condition"
                           checked
+                          onInput={event => setCourseStatus(event.target.value)}
                         />
                       </label>
                     </div>
                     <div class="unavailable">
                       <label>
-                        <span>ناموجود</span>
-                        <input
-                          type="radio"
-                          value="unavailable"
-                          name="condition"
-                        />
+                        <span>پیش فروش</span>
+                        <input type="radio" value="presell" name="condition"
+                        onInput={event => setCourseStatus(event.target.value)} />
                       </label>
                     </div>
                   </div>
                 </div>
                 <div class="submit-btn">
-                  <input type="submit" value="افزودن" />
-                </div>
-              </div>
-            </div>
-            <div class="col-6">
-              <div class="presell">
-                <label class="input-title">وضعیت دوره</label>
-                <div class="radios">
-                  <div class="presell-true">
-                    <label>
-                      <span>پیش فروش</span>
-                      <input
-                        type="radio"
-                        value="presell"
-                        name="presell"
-                        checked
-                      />
-                    </label>
-                  </div>
-                  <div class="presell-false">
-                    <label>
-                      <span>در حال برگزاری</span>
-                      <input type="radio" value="onperforming" name="presell" />
-                    </label>
-                  </div>
+                  <input type="submit" value="افزودن" onClick={addNewCourse} />
                 </div>
               </div>
             </div>
           </form>
         </div>
       </div>
-   <DataTable title='دوره ها'>
-   <table class="table">
+      <DataTable title="دوره ها">
+        <table class="table">
           <thead>
             <tr>
               <th>شناسه</th>
@@ -209,8 +254,14 @@ const localStorageData = JSON.parse(localStorage.getItem('user'))
               <tr>
                 <td>{index + 1}</td>
                 <td>{course.name}</td>
-                <td>{course.price === 0 ? 'رایگان' : course.price.toLocaleString()}</td>
-                <td>{course.isComplete === 0 ? 'درحال برگذاری' : 'تکمیل شده'}</td>
+                <td>
+                  {course.price === 0
+                    ? "رایگان"
+                    : course.price.toLocaleString()}
+                </td>
+                <td>
+                  {course.isComplete === 0 ? "درحال برگذاری" : "تکمیل شده"}
+                </td>
                 <td>{course.shortName}</td>
                 <td>{course.creator}</td>
                 <td>{course.categoryID.name}</td>
@@ -223,7 +274,7 @@ const localStorageData = JSON.parse(localStorage.getItem('user'))
                   <button
                     type="button"
                     class="btn btn-danger delete-btn"
-                   onClick={() => removeCourse(course._id)}
+                    onClick={() => removeCourse(course._id)}
                   >
                     حذف
                   </button>
@@ -232,7 +283,7 @@ const localStorageData = JSON.parse(localStorage.getItem('user'))
             ))}
           </tbody>
         </table>
-   </DataTable>
-   </>
-  )
+      </DataTable>
+    </>
+  );
 }
