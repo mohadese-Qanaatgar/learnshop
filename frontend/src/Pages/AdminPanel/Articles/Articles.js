baf5c2bcd4a45d47e3fe343125b84ae09387d3c6
unknown <m.qanaatgar@gmail.com>
2024-02-22 00:08:59 +0330

    show articles list

--- frontend/src/Pages/AdminPanel/Articles/Articles.js
+++ frontend/src/Pages/AdminPanel/Articles/Articles.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import DataTable from "../DataTable/DataTable";
 
 export default function Articles() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+    fetch(`http://localhost:4000/v1/articles`)
+      .then((res) => res.json())
+      .then((allArticles) => {
+        setArticles(allArticles);
+      });
+  }, []);
   return (
-    <div>Articles</div>
-  )
+    <>
+      <DataTable title="مقاله ها">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+              <th>لینک</th>
+              <th>نویسنده</th>
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles.map((article, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{article.title}</td>
+                <td>{article.shortName}</td>
+                <td>{article.creator.name}</td>
+                <td>
+                  <button type="button" class="btn btn-primary edit-btn">
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-danger delete-btn"
+                    // onClick={() => removeCourse(course._id)}
+                  >
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DataTable>
+    </>
+  );
 }
