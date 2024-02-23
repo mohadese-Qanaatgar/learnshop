bfd836fef93850e9ef3056e9f4ba073a23977d78
unknown <m.qanaatgar@gmail.com>
2024-02-23 23:55:02 +0330

    prepare create menu form

--- frontend/src/Pages/AdminPanel/Menus/Menus.js
+++ frontend/src/Pages/AdminPanel/Menus/Menus.js
@@ -1,78 +1,163 @@
-import React, { useEffect, useState } from 'react'
-import DataTable from '../DataTable/DataTable'
-import swal from 'sweetalert'
+import React, { useEffect, useState } from "react";
+import DataTable from './../DataTable/DataTable'
+import swal from "sweetalert";
+import { useForm } from "./../../../hooks/useForm";
+import Input from "./../../../Components/Form/Input";
+import { minValidator } from "../../../Validators/rules";
 
 export default function Menus() {
+  const [menus, setMenus] = useState([]);
+  const [menuParent, setMenuParent] = useState("-1");
 
-  const [menus , setMenus] = useState([])
+  const [formState, onInputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      href: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
   useEffect(() => {
-    getAllMenus()
-  },[])
+    getAllMenus();
+  }, []);
 
-  function getAllMenus () {
-    const localStorageData = JSON.parse(localStorage.getItem('user'))
-
-    fetch(`http://localhost:4000/v1/menus/all`)
-    .then(res => res.json())
-    .then(allMenus => {
-      setMenus(allMenus)
-    })
+  function getAllMenus() {
+    fetch("http://localhost:4000/v1/menus/all")
+      .then((res) => res.json())
+      .then((allMenus) => setMenus(allMenus));
   }
 
   const removeMenu = (menuID) => {
-    const localStorageData = JSON.parse(localStorage.getItem('user'))
-
     swal({
-      title : 'آیا از حذف مطمدن هستید؟',
-      icon : 'warning',
-      buttons : ["نه" , "آره"]
-    }).then(result => {
-      if(result) {
-        fetch(`http://localhost:4000/v1/menus/${menuID}` , {
-          method : 'DELETE',
-          headers : {
-            Authorization : `Bearer ${localStorageData.token}`
-          }
-        }).then(res => {
-          if(res.ok) {
+      title: "آیا از حذف منو اطمینان دارید؟",
+      icon: "warning",
+      buttons: ["نه", "آره"],
+    }).then((result) => {
+      if (result) {
+        fetch(`http://localhost:4000/v1/menus/${menuID}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("user")).token
+            }`,
+          },
+        }).then((res) => {
+          if (res.ok) {
             swal({
-              title : "منو با موفقیت حذف شد",
-              icon : 'success',
-              buttons : 'تایید'
-            }).then(() => {getAllMenus()})
+              title: "منوی مورد نظر با موفقیت حذف شد",
+              icon: "success",
+              buttons: "اوکی",
+            }).then(() => {
+              getAllMenus();
+            });
           }
-        })
+        });
       }
-    })
+    });
+  };
 
-    
-  }
+  const createMenu = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-    <DataTable title='منوها'>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>شناسه</th>
-            <th>عنوان</th>
-            <th>مقصد</th>
-            <th>فرزند...</th>
-            <th>ویرایش</th>
-            <th>حذف</th>
-          </tr>
-        </thead>
-        <tbody>
-          {menus.map((menu , index) => (
+      <div class="container">
+        <div class="home-title">
+          <span>افزودن کاربر جدید</span>
+        </div>
+        <form class="form">
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">عنوان</label>
+              <Input
+                element="input"
+                onInputHandler={onInputHandler}
+                id="title"
+                type="text"
+                isValid="false"
+                placeholder="لطفا عنوان را وارد کنید..."
+                validations={[minValidator(5)]}
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">عنوان</label>
+              <Input
+                element="input"
+                onInputHandler={onInputHandler}
+                id="href"
+                type="text"
+                isValid="false"
+                validations={[minValidator(5)]}
+                placeholder="لطفا عنوان را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">عنوان</label>
+              <select
+                class="select"
+                onChange={(event) => setMenuParent(event.target.value)}
+              >
+                <option value="-1">منوی اصلی را انتخاب کنید</option>
+                {menus.map((menu) => (
+                  <>
+                    {!Boolean(menu.parent) && (
+                      <option value={menu._id}>{menu.title}</option>
+                    )}
+                  </>
+                ))}
+              </select>
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="bottom-form">
+              <div class="submit-btn">
+                <input type="submit" value="افزودن" onClick={createMenu} />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <DataTable title="منوها">
+        <table class="table">
+          <thead>
             <tr>
-              <td>{index + 1}</td>
-              <td>{menu.title}</td>
-              <td>{menu.href}</td>
-              <td>{
-                menu.parent ? menu.parent.title : (<i className='fa fa-check'></i>)
-                }</td>
-              <td>
+              <th>شناسه</th>
+              <th>عنوان</th>
+              <th>مقصد</th>
+              <th>فرزند ...</th>
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {menus.map((menu, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{menu.title}</td>
+                <td>{menu.href}</td>
+                <td>
+                  {menu.parent ? (
+                    menu.parent.title
+                  ) : (
+                    <i className="fa fa-check"></i>
+                  )}
+                </td>
+                <td>
                   <button type="button" class="btn btn-primary edit-btn">
                     ویرایش
                   </button>
@@ -86,11 +171,11 @@ export default function Menus() {
                     حذف
                   </button>
                 </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </DataTable>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DataTable>
     </>
-  )
+  );
 }
