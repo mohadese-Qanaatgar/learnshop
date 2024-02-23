25f86f335809356797390b1b83bcdd12fec38dcc
unknown <m.qanaatgar@gmail.com>
2024-02-23 14:11:02 +0330

    create session

--- frontend/src/Pages/AdminPanel/Sessions/Sessions.js
+++ frontend/src/Pages/AdminPanel/Sessions/Sessions.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "./../../../hooks/useForm";
 import Input from "./../../../Components/Form/Input";
 import { minValidator } from "../../../Validators/rules";
+import swal from 'sweetalert'
 
 export default function Sessions() {
   const [courses, setCourses] = useState([]);
@@ -25,10 +26,40 @@ export default function Sessions() {
     fetch("http://localhost:4000/v1/courses")
       .then((res) => res.json())
       .then((allCourses) => {
-        console.log(allCourses);
+        // console.log(allCourses);
         setCourses(allCourses);
       });
   }, []);
+
+  const createSession = (event) => {
+    event.preventDefault()
+    const localStorageData = JSON.parse(localStorage.getItem('user'))
+    let formData = new FormData()
+
+    formData.append('title' , formState.inputs.title.value)
+    formData.append('time' , formState.inputs.time.value)
+    formData.append('video' , sessionVideo)
+    formData.append('free' , formState.inputs.free.value)
+
+
+    fetch(`http://localhost:4000/v1/courses/${sessionCourse}/sessions`, {
+        method : 'POST',
+        headers : {
+            Authorization : `Bearer ${localStorageData.token}`
+        },
+        body : formData
+    }).then(res => {
+        if(res.ok) {
+            swal({
+                title:'جلسه با موفقیت اضافه شد',
+                icon : 'success',
+                buttons : 'تایید'
+            }).then(() => {
+                console.log('ok');
+            })
+        }
+    })
+}
 
   return (
     <>
@@ -90,7 +121,7 @@ export default function Sessions() {
             <div class="col-12">
               <div class="bottom-form">
                 <div class="submit-btn">
-                  <input type="submit" value="افزودن" />
+                  <input type="submit" value="افزودن" onClick={createSession} />
                 </div>
               </div>
             </div>
