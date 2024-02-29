e089a01ab3f7b62b812f3d402e92d31d38aaf5f7
unknown <m.qanaatgar@gmail.com>
2024-02-29 17:35:01 +0330

    show all disconnect

--- frontend/src/Pages/AdminPanel/Offs/Offs.js
+++ frontend/src/Pages/AdminPanel/Offs/Offs.js
@@ -1,75 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import Input from '../../../Components/Form/Input'
-import { useForm } from '../../../hooks/useForm'
-import { minValidator, requiredValidator  } from '../../../Validators/rules'
-import swal from 'sweetalert'
+import React, { useEffect, useState } from "react";
+import Input from "../../../Components/Form/Input";
+import { useForm } from "../../../hooks/useForm";
+import { minValidator, requiredValidator } from "../../../Validators/rules";
+import swal from "sweetalert";
+import DataTable from "../DataTable/DataTable";
 
 export default function Offs() {
-    const [courses , setCourses] = useState([])
-    const [offCourse , setOffCourse] = useState('-1')
-    const [formState, onInputHandler] = useForm(
-        {
-            code: {
-                value: "",
-                isValid: false,
-              },
-              percent: {
-                value: "",
-                isValid: false,
-              },
-              max: {
-                value: "",
-                isValid: false,
-              }
-        },
-        false
-      );
+  const [courses, setCourses] = useState([]);
+  const [offs, setOffs] = useState([]);
+  const [offCourse, setOffCourse] = useState("-1");
+  const [formState, onInputHandler] = useForm(
+    {
+      code: {
+        value: "",
+        isValid: false,
+      },
+      percent: {
+        value: "",
+        isValid: false,
+      },
+      max: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
-      useEffect(() => {
-        getAllCourses()
-      },[])
+  useEffect(() => {
+    getAllOffs();
 
-    function getAllCourses () {
-        fetch(`http://localhost:4000/v1/courses`)
-        .then(res => res.json())
-        .then(allCourses => {
-            setCourses(allCourses)
-        })
-    }
-    const createOff = (event) => {
-        event.preventDefault()
-        const localStorageData = JSON.parse(localStorage.getItem('user'))
+    fetch(`http://localhost:4000/v1/courses`)
+      .then((res) => res.json())
+      .then((allCourses) => {
+        setCourses(allCourses);
+      });
+  }, []);
 
-        const newOffInfos = {
-            code : formState.inputs.code.value,
-            percent : formState.inputs.percent.value,
-            course : offCourse,
-            max : formState.inputs.max.value
-        }
+  function getAllOffs() {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+    fetch(`http://localhost:4000/v1/offs`, {
+      headers: {
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((allOffs) => {
+        // console.log(allOffs);
+        setOffs(allOffs);
+      });
+  }
+  const createOff = (event) => {
+    event.preventDefault();
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
 
-        fetch(`http://localhost:4000/v1/offs` , {
-            method : 'POST',
-            headers : {
-                'Content-Type' : 'application/json',
-                Authorization : `Bearer ${localStorageData.token}`
-            },
-            body : JSON.stringify(newOffInfos)
-        }).then(res => {
-            console.log(res);
-            if(res.ok){
-                swal({
-                    title : 'کد تخفیف با موفقیت ایجاد شد',
-                    icon : 'success',
-                    buttons : 'تایید'
-                }).then(() => {
-                    // getAllOffs()
-                })
-            }
-        })
-    }
+    const newOffInfos = {
+      code: formState.inputs.code.value,
+      percent: formState.inputs.percent.value,
+      course: offCourse,
+      max: formState.inputs.max.value,
+    };
+
+    fetch(`http://localhost:4000/v1/offs`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
+      body: JSON.stringify(newOffInfos),
+    }).then((res) => {
+    //   console.log(res);
+      if (res.ok) {
+        swal({
+          title: "کد تخفیف با موفقیت ایجاد شد",
+          icon: "success",
+          buttons: "تایید",
+        }).then(() => {
+          getAllOffs();
+        });
+      }
+    });
+  };
   return (
     <>
-    <div class="container-fluid" id="home-content">
+      <div class="container-fluid" id="home-content">
         <div class="container">
           <div class="home-title">
             <span>افزودن جلسه جدید</span>
@@ -116,32 +130,73 @@ export default function Offs() {
                 />
                 <span class="error-message text-danger"></span>
               </div>
-              </div>
+            </div>
             <div class="col-6">
               <div class="price input">
                 <label class="input-title" style={{ display: "block" }}>
                   دوره
                 </label>
-                <select class="select" onClick={event => setOffCourse(event.target.value)}>
-                    <option value="-1">دوره مدنظر را انتخاب کنید</option>
-                    {courses.map(course => (
-                        <option key={course._id} value={course._id}>{course.name}</option>
-                    ))}
+                <select
+                  class="select"
+                  onClick={(event) => setOffCourse(event.target.value)}
+                >
+                  <option value="-1">دوره مدنظر را انتخاب کنید</option>
+                  {courses.map((course) => (
+                    <option key={course._id} value={course._id}>
+                      {course.name}
+                    </option>
+                  ))}
                 </select>
                 <span class="error-message text-danger"></span>
               </div>
             </div>
-          
+
             <div class="col-12">
               <div class="bottom-form">
                 <div class="submit-btn">
-                  <input type="submit" value="افزودن" onClick={createOff}/>
+                  <input type="submit" value="افزودن" onClick={createOff} />
                 </div>
               </div>
             </div>
           </form>
         </div>
       </div>
+      <DataTable title="کدهای تخفیف">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>کد</th>
+              <th>درصد</th>
+              <th>سازنده</th>
+              <th>حداکثر استفاده</th>
+              <th>دفعات استفاده</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {offs.map((off, index) => (
+              <tr key={off._id}>
+                <td>{index + 1}</td>
+                <td>{off.code}</td>
+                <td>{off.percent}</td>
+                <td>{off.creator}</td>
+                <td>{off.max}</td>
+                <td>{off.uses}</td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-danger delete-btn"
+                    // onClick={() => removeComment(comment._id)}
+                  >
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DataTable>
     </>
-  )
+  );
 }
