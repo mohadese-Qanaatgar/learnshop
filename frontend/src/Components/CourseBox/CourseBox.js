2bc55440f2be7b14a95a49632dab6320cbe342eb
unknown <m.qanaatgar@gmail.com>
2024-03-01 15:24:50 +0330

    show course avg score

--- frontend/src/Components/CourseBox/CourseBox.js
+++ frontend/src/Components/CourseBox/CourseBox.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import './CourseBox.css';
-import CircleSpinner from '../CircleSpinner/CircleSpinner';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./CourseBox.css";
+import CircleSpinner from "../CircleSpinner/CircleSpinner";
+import { Link } from "react-router-dom";
 
 export default function CourseBox(props) {
-
-  const [isShowImage , setIsShowImage] = useState(false)
+  const [isShowImage, setIsShowImage] = useState(false);
 
   const onLoadImage = () => {
-    setIsShowImage(true)
-  }
+    setIsShowImage(true);
+  };
 
   return (
-    <div className="col-4" style={{width :`${props.isSlider && '100%'}`}}>
+    <div className="col-4" style={{ width: `${props.isSlider && "100%"}` }}>
       <div className="course-box">
         <Link to={`/course-info/${props.shortName}`}>
           <img
@@ -24,7 +23,10 @@ export default function CourseBox(props) {
           {!isShowImage && <CircleSpinner />}
         </Link>
         <div className="course-box__main">
-          <Link to={`/course-info/${props.shortName}`} className="course-box__title">
+          <Link
+            to={`/course-info/${props.shortName}`}
+            className="course-box__title"
+          >
             {props.name}
           </Link>
 
@@ -36,31 +38,25 @@ export default function CourseBox(props) {
               </a>
             </div>
             <div className="course-box__rating">
-              <img
-                src="/images/svgs/star.svg"
-                alt="rating"
-                className="course-box__star"
-              />
-              <img
-                src="/images/svgs/star_fill.svg"
-                alt="rating"
-                className="course-box__star"
-              />
-              <img
-                src="/images/svgs/star_fill.svg"
-                alt="rating"
-                className="course-box__star"
-              />
-              <img
-                src="/images/svgs/star_fill.svg"
-                alt="rating"
-                className="course-box__star"
-              />
-              <img
-                src="/images/svgs/star_fill.svg"
-                alt="rating"
-                className="course-box__star"
-              />
+              {
+                Array(5 - props.courseAverageScore).fill(0).map(item => (
+                  <img
+                  src='/images/svgs/star.svg' 
+                  alt="rating"
+                  className="course-box__star"
+                />
+                ))
+              }
+              {
+                Array(props.courseAverageScore).fill(0).map(item => (
+                  <img
+                  src='/images/svgs/star_fill.svg' 
+                  alt="rating"
+                  className="course-box__star"
+                />
+                ))
+              }
+             
             </div>
           </div>
 
@@ -70,17 +66,16 @@ export default function CourseBox(props) {
               <span className="course-box__users-text">{props.registers}</span>
             </div>
             <span className="course-box__price">
-              {props.price === 0 ? (
-                'رایگان'
-              ) : (
-                props.price.toLocaleString()
-              )}
+              {props.price === 0 ? "رایگان" : props.price.toLocaleString()}
             </span>
           </div>
         </div>
 
         <div className="course-box__footer">
-          <Link to={`/course-info/${props.shortName}`} className="course-box__footer-link">
+          <Link
+            to={`/course-info/${props.shortName}`}
+            className="course-box__footer-link"
+          >
             مشاهده اطلاعات
             <i className="fas fa-arrow-left course-box__footer-icon"></i>
           </Link>
