879318c486f618ee0db594a70fce0408744fba48
unknown <m.qanaatgar@gmail.com>
2024-03-08 13:39:43 +0330

    show tab menu

--- frontend/src/Pages/UserPanel/Courses/Courses.js
+++ frontend/src/Pages/UserPanel/Courses/Courses.js
@@ -4,6 +4,8 @@ import "./Courses.css";
 
 export default function Courses() {
   const [courses, setCourses] = useState([]);
+  const [shownCourses, setShownCourses] = useState([]);
+  const [showCourseState, setShowCourseState] = useState("all");
 
   useEffect(() => {
     fetch(`http://localhost:4000/v1/users/courses/`, {
@@ -16,8 +18,31 @@ export default function Courses() {
       .then((res) => res.json())
       .then((data) => {
         setCourses(data);
+        setShownCourses(data)
       });
   }, []);
+
+  const filterCourses = (state) => {
+    switch(state) {
+      case 'all' : {
+        setShownCourses(courses)
+        break
+      }
+      case 'free' : {
+        const filteredCourses = courses.filter(course =>  course.course.price === 0)
+        setShownCourses(filteredCourses)
+        break
+      }
+      case 'money' : {
+        const filteredCourses = courses.filter(course =>  course.course.price !== 0)
+        setShownCourses(filteredCourses)
+        break
+      }
+      default : {
+        setShownCourses(courses)
+      }
+    }
+  }
 
   return (
     <div class="col-9">
@@ -25,22 +50,55 @@ export default function Courses() {
         <div class="courses-header">
           <span class="courses-header__title">دوره های ثبت نام شده</span>
           <ul class="courses-header__list">
-            <li class="courses-header__item">
+            <li class="courses-header__item"
+            onClick={(event) => {
+               event.preventDefault()
+               filterCourses('all')
+              setShowCourseState('all')}}
+            >
               <a
-                class="courses-header__link courses-header__link-active"
+                class={`courses-header__link ${
+                  showCourseState === "all"
+                    ? "courses-header__link-active"
+                    : null
+                }`}
                 href="#"
               >
                 همه دوره ها
               </a>
             </li>
-            <li class="courses-header__item">
-              <a class="courses-header__link" href="#">
-                دوره های فعال
+            <li class="courses-header__item"
+            onClick={(event) => {
+               event.preventDefault()
+               filterCourses('free')
+              setShowCourseState("free")}}
+            >
+              <a
+                class={`courses-header__link ${
+                  showCourseState === "free"
+                    ? "courses-header__link-active"
+                    : null
+                }`}
+                href="#"
+              >
+                دوره های رایگان
               </a>
             </li>
-            <li class="courses-header__item">
-              <a class="courses-header__link" href="#">
-                دوره های تکمیل شده
+            <li class="courses-header__item"
+            onClick={(event) => {
+               event.preventDefault()
+               filterCourses('money')
+              setShowCourseState('money')}}
+            >
+              <a
+                class={`courses-header__link ${
+                  showCourseState === "money"
+                    ? "courses-header__link-active"
+                    : null
+                }`}
+                href="#"
+              >
+                دوره های پولی
               </a>
             </li>
           </ul>
@@ -48,30 +106,36 @@ export default function Courses() {
         <div class="main">
           <div class="row">
             <div class="col-12">
-              {courses.map((course) => (
+              {shownCourses.map((course) => (
                 <div class="main__box">
                   <div class="main__box-right">
                     <a class="main__box-img-link" href="#">
                       <img
                         class="main__box-img img-fluid"
-                        src="images/courses/js_project.png"
+                        src={`http://localhost:4000/courses/covers/${course.course.cover}`}
                       />
                     </a>
                   </div>
                   <div class="main__box-left">
                     <a href="#" class="main__box-title">
-                      آموزش جاوا اسکریپت
+                      {course.course.name}
                     </a>
                     <div class="main__box-bottom">
                       <div class="main__box-all">
-                        <span class="main__box-all-text">کل دروس:</span>
-                        <span class="main__box-all-value">75</span>
+                        <span class="main__box-all-text">وضعیت:</span>
+                        <span class="main__box-all-value">
+                          {course.course.isComplete === 1
+                            ? "تکمیل شده"
+                            : "در حال برگذاری"}
+                        </span>
                       </div>
                       <div class="main__box-completed">
-                        <span class="main__box-completed-text">
-                          دروس تکمیل شده:
+                        <span class="main__box-completed-text">مبلغ :</span>
+                        <span class="main__box-completed-value">
+                          {course.course.price === 0
+                            ? " رایگان"
+                            : course.course.price}
                         </span>
-                        <span class="main__box-completed-value">0/75</span>
                       </div>
                     </div>
                   </div>
